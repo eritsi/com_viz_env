e8c6a5f10f8fe661fead636cf9b95de1b0d4bd2c
eritsi <eritsi@gmail.com>
2021-07-21 14:15:42 +0000

    initial commit

---
+++ .github/workflows/scripts/replace.sh
@@ -0,0 +1,4 @@
+# Tuckle dscc issue #190 according to https://github.com/googledatastudio/tooling/issues/190
+cp ../.github/workflows/scripts/util.js ./node_modules/@google/dscc-scripts/build/viz/util.js
+cp ../.github/workflows/scripts/build.js ./node_modules/@google/dscc-scripts/build/viz/build.js
+cp ../.github/workflows/scripts/message.js ./node_modules/@google/dscc-scripts/build/viz/message.js
